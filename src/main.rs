5bdea3260f8116dc957b1c68f6f552e2a4483824
thanhlam <lamdoan1122334455@gmail.com>
2025-05-28 09:51:35 +0700

    if else

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    println!("Hello, world!");
 
-    println!("Please enter an array index:");
-
-    let mut index = String::new();
+    let mut number = String::new();
+    
+    println!("Please enter a number:");
 
     std::io::stdin()
-        .read_line(&mut index)
+        .read_line(&mut number)
         .expect("Failed to read line");
-
-    let index: usize = index.trim().parse().expect("Failed to parse integer");
     
-    println!("Value is: {}", a[index]);
+    let number: u32 = number.trim().parse().expect("Please type a number!");
+    
+    div(number);
+}
+
+fn test() {
+    println!("Day la ham test");
+}
+
+fn test2() -> i32 {
+    return 5;
+}
+
+fn ifelse(number: i32) {
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+fn div(number: u32) {
+    if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 5 == 0 {
+        println!("number is divisible by 5");
+    } else {
+        println!("number is not divisible by 2, 3 or 5");
+    }
 }
