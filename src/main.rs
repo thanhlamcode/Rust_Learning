76963a973f6b9e754bc28c55bc41575bb7cbc0e4
thanhlam <lamdoan1122334455@gmail.com>
2025-05-28 17:02:12 +0700

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,35 @@
 fn main() {
-
-    //1. Mỗi giá trị trong Rust có một chủ sở hữu (owner).
-    let s = String::from("Hello, world!"); // s là chủ sở hữu của chuỗi "Hello, world!"
-    println!("{}", s); // Sử dụng s
     
-    // 2. Chỉ có một chủ sở hữu tại một thời điểm.
-    let s1 = String::from("Hello, world!");
-    let s2 = s1; // s1 chuyển quyền sở hữu sang s2
 
-    println!("{}", s2); // s2 sở hữu chuỗi này, không còn s1
-    // println!("{}", s1); // Lỗi biên dịch: `s1` đã bị chuyển nhượng quyền sở hữu, không thể sử dụng lại
+    fn area(rectangle: &Rectangle) -> u32 {
+        rectangle.width * rectangle.height
+    }
 
-    //3. Khi chủ sở hữu ra khỏi phạm vi (scope), giá trị sẽ bị giải phóng (dropped).
-    {
-        let s = String::from("Hello, world!"); // s là chủ sở hữu của chuỗi này trong phạm vi nhỏ
-    } // Khi ra khỏi phạm vi, s bị giải phóng và bộ nhớ bị giải phóng
+    println!("Please enter width:");
+    let mut width_input = String::new();
+    std::io::stdin()
+        .read_line(&mut width_input)
+        .expect("Failed to read line");
+    println!("Please enter height:");
+    let mut height_input = String::new();
+    std::io::stdin()
+        .read_line(&mut height_input)
+        .expect("Failed to read line");
+    let width: u32 = width_input.trim().parse().expect("Please enter width");
+    let height: u32 = height_input.trim().parse().expect("Please type a number");
 
-    // Không thể sử dụng s ngoài phạm vi nó được khai báo
+    let rectangle = Rectangle {
+        width,
+        height,
+    };
+    
+    println!("Area is {}", area(&rectangle));
+    
+    println!("{:#?}", rectangle)
 }
 
-
-// fn condition(){
-//     let condition = true;
-//
-//     let number = if condition { 5 } else { 6 };
-//
-//     println!("The value of number is: {}", number);
-// }
-
-// fn test() {
-//     println!("Day la ham test");
-// }
-//
-// fn test2() -> i32 {
-//     return 5;
-// }
-//
-// fn ifelse(number: i32) {
-//     if number < 5 {
-//         println!("condition was true");
-//     } else {
-//         println!("condition was false");
-//     }
-// }
-//
-// fn div(number: u32) {
-//     if number % 2 == 0 {
-//         println!("number is divisible by 2");
-//     } else if number % 3 == 0 {
-//         println!("number is divisible by 3");
-//     } else if number % 5 == 0 {
-//         println!("number is divisible by 5");
-//     } else {
-//         println!("number is not divisible by 2, 3 or 5");
-//     }
-// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
