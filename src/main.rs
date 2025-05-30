d90b7243208c36e5b69b5d6d17b2019aea91395a
thanhlam <lamdoan1122334455@gmail.com>
2025-05-30 17:08:51 +0700

    string collection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-
 use guessing_game::phone;
+use unicode_segmentation::UnicodeSegmentation;
+
 
 fn main() {
-    phone::list_phone();
+    // phone::list_phone();
+
+    let s = String::from("Hôm nay là thứ ");
+
+    let x = s.chars().nth(6).unwrap();
+    let y = s.bytes().nth(6).unwrap();
+    println!("{}", x);
+    println!("{}", y);
 }
