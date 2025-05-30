11cc735196ebac1f8f191f33f835b260ed889383
thanhlam <lamdoan1122334455@gmail.com>
2025-05-30 17:58:36 +0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use guessing_game::phone;
-use unicode_segmentation::UnicodeSegmentation;
-
+use std::collections::HashMap;
 
 fn main() {
-    // phone::list_phone();
+    let v = vec![1, 2, 3, 4, 5, 6, 1, 2, 2, 1, 1, 2, 4, 2, 5, 3, 4, 2, 6];
 
-    let s = String::from("Hôm nay là thứ ");
 
-    let x = s.chars().nth(6).unwrap();
-    let y = s.bytes().nth(6).unwrap();
-    println!("{}", x);
-    println!("{}", y);
+    let median = v[v.len() / 2];
+    println!("median: {}", median);
+
+    let mut counts = HashMap::new();
+
+    for nums in v {
+        *counts.entry(nums).or_insert(0) += 1;
+    }
+    println!("counts: {:?}", counts);
+
+    let mode = counts.into_iter().max_by_key(|&(_, count)| count).unwrap();
+
+    println!("mode: {:?}", mode);
 }
