f2d4327b57d874bb1f459f1b528a196b9c712555
thanhlam <lamdoan1122334455@gmail.com>
2025-06-02 10:46:30 +0700

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,44 @@
-use std::fs::File;
+// use std::fs::File;
 
 fn main() {
-    fn safe_divide(a: i32, b: i32) -> Result<i32, String> {
-        if b == 0 {
-            return Err("Lỗi: Không thể chia cho 0".to_string());
+    // fn safe_divide(a: i32, b: i32) -> Result<i32, String> {
+    //     if b == 0 {
+    //         return Err("Lỗi: Không thể chia cho 0".to_string());
+    //     }
+    //
+    //     Ok(a / b)
+    // }
+    //
+    // match safe_divide(45, 0) {
+    //     Ok(n) => println!("{}", n),
+    //     Err(msg) => println!("{}", msg),
+    // }
+    //
+    // fn bmi_calculator(height: f32, weight: f32) -> Result<f32, String> {
+    //     if height == 0.0 {
+    //         return Err("Loi".to_string());
+    //     }
+    //     Ok(weight / (height * height))
+    // }
+    //
+    // let bmi = bmi_calculator(60.0, 1.8);
+    // match bmi {
+    //     Ok(n) => println!("{}", n),
+    //     Err(msg) => println!("{}", msg),
+    // }
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        println!("{} and {}", number, largest);
+        if number > largest {
+            largest = number;
         }
-
-        Ok(a / b)
+        println!("Largest number is {}", largest);
+        println!("========================");
     }
 
-    match safe_divide(45, 0) {
-        Ok(n) => println!("{}", n),
-        Err(msg) => println!("{}", msg),
-    }
-
-    fn bmi_calculator(height: f32, weight: f32) -> Result<f32, String> {
-        if height == 0.0 {
-            return Err("Loi".to_string());
-        }
-        Ok(weight / (height * height))
-    }
-
-    let bmi = bmi_calculator(60.0, 1.8);
-    match bmi {
-        Ok(n) => println!("{}", n),
-        Err(msg) => println!("{}", msg),
-    }
+    println!("{}", largest);
 }
